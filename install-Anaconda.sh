eac03f9b3cb0f1e4ab3a3a1945d04de54e2c1fef
sizuki38 <77662856+sizuki38@users.noreply.github.com>
2021-02-12 15:52:07 +0900

    Add files via upload

--- install-Anaconda.sh
+++ install-Anaconda.sh
@@ -4,6 +4,7 @@ sudo apt upgrade
 wget https://repo.anaconda.com/archive/Anaconda3-2020.11-Linux-x86_64.sh
 bash Anaconda3-2020.11-Linux-x86_64.sh
 rm Anaconda3-2020.11-Linux-x86_64.sh
+shopt -s expand_aliases
 source ~/.bashrc
 jupyter notebook --generate-config
 echo c.NotebookApp.ip = '0.0.0.0'>>.jupyter/jupyter_notebook_config.py
