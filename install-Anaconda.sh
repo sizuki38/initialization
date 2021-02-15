09104a5fe2e696877cdd552cf11dd9da259e7084
sizuki38 <77662856+sizuki38@users.noreply.github.com>
2021-02-15 14:24:41 +0900

    Update install-Anaconda.sh

--- install-Anaconda.sh
+++ install-Anaconda.sh
@@ -4,7 +4,7 @@ sudo apt upgrade
 wget https://repo.anaconda.com/archive/Anaconda3-2020.11-Linux-x86_64.sh
 bash Anaconda3-2020.11-Linux-x86_64.sh
 rm Anaconda3-2020.11-Linux-x86_64.sh
-. ~/.bashrc
+export PATH=~/anconda3/bin:$PATH
 jupyter notebook --generate-config
 echo c.NotebookApp.ip = '0.0.0.0'>>.jupyter/jupyter_notebook_config.py
 jupyter-notebook
