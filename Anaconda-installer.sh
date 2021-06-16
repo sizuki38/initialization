521e14ea9440da024a3d57a0f4fab5583c578bec
sizuki38 <mu3ic8@gmail.com>
2021-06-16 13:04:18 +0900

    rename

---
+++ Anaconda-installer.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+sudo apt update
+sudo apt upgrade
+wget https://repo.anaconda.com/archive/Anaconda3-2020.11-Linux-x86_64.sh
+bash Anaconda3-2020.11-Linux-x86_64.sh
+rm Anaconda3-2020.11-Linux-x86_64.sh
+export PATH=~/anaconda3/bin:'$PATH'
+echo 'export PATH=~/anconda3/bin:$PATH'>>.bashrc
+echo source ~/.bashrc
+source ~/.bashrc
+echo jupyter notebook --generate-config
+jupyter notebook --generate-config
+echo 'c.NotebookApp.ip = '0.0.0.0'>>.jupyter/jupyter_notebook_config.py'
+echo c.NotebookApp.ip = "'"0.0.0.0"'">>.jupyter/jupyter_notebook_config.py
+jupyter-notebook
+
