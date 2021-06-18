875230ac99fdce43cd166431b92507f4609bee5d
sizuki38 <c0117032f2@edu.teu.ac.jp>
2021-06-18 18:51:24 +0900

    remote jupyterLab

--- Anaconda-installer.sh
+++ Anaconda-installer.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sudo apt update
-sudo apt upgrade
+sudo apt -y upgrade
 wget https://repo.anaconda.com/archive/Anaconda3-2020.11-Linux-x86_64.sh
 bash Anaconda3-2020.11-Linux-x86_64.sh
 rm Anaconda3-2020.11-Linux-x86_64.sh
@@ -8,9 +8,8 @@ export PATH=~/anaconda3/bin:'$PATH'
 echo 'export PATH=~/anconda3/bin:$PATH'>>.bashrc
 echo source ~/.bashrc
 source ~/.bashrc
+cd
 echo jupyter notebook --generate-config
-jupyter notebook --generate-config
-echo 'c.NotebookApp.ip = '0.0.0.0'>>.jupyter/jupyter_notebook_config.py'
+jupyter-notebook --generate-config
 echo c.NotebookApp.ip = "'"0.0.0.0"'">>.jupyter/jupyter_notebook_config.py
 jupyter-notebook
-
